2cce53f804d0456af98ec6f5fe1820287145f948
Sohier Christie <sohier@scsquared.ca>
2013-12-05 12:35:51 -0400

    first commit

---
+++ contact-form.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if (gt IE 8)]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+  <title>SC&#178;.create. | Contact</title>
+  <meta content="photography illustration and consulting" name="description">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="yes" name="apple-mobile-web-app-capable">
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="javascripts/PIE.js"></script>
+  <![endif]-->
+  <link href="favicon.ico" rel="shortcut icon">
+  <link href="stylesheets/style.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
+</head>
+<body>
+  <div class="wrapper">
+    <!-- Page Header -->
+<header id="masthead">
+    <div class="navbar transparent navbar-fixed-top">
+      <div class="navbar-inner">
+          <div class="container">
+            <a class="btn btn-navbar" data-target=".nav-collapse" data-toggle="collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </a> 
+            <h1 class="brand">
+              <a href="index.html">
+                SC<span class="light">&#178;.create.</h1></span></a>
+            <div class="nav-collapse collapse">
+              <ul class="nav pull-right">
+                <li class=""><a href="./">Home</a></li>
+                <li class="dropdown">
+                  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Photography<i class="caret"></i></a>
+                  <ul class="dropdown-menu">
+                  	<li><a href="product.html">Product Photography</a></li>
+                    <li><a href="weddings.html">Weddings</a></li>
+                    <li><a href="fineartphoto.html">Fine Art</a></li>
+                  </ul>
+                </li>
+               <li class=><a href="illfolio.html">Illustrations</a></li>
+                <li class="dropdown active">
+                	<a class="dropdown-toggle" data-toggle="dropdown" href="#">About<i class="caret"></i></a>
+                	<ul class="dropdown-menu">
+                    <li><a href="windward.html">Windward Flutes</a></li>
+                	<li class="active"><a href="contact.html">Contact</a></li>
+                	<li><a href="about.html">About</a></li>
+					</ul>
+                </li>
+                	</ul>
+                </li>
+
+                </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- Main Content -->
+    <div id="content" role="main">
+      <!-- Promo Section -->
+
+      <!-- 404 content -->
+      <section class="section section-padded">
+        <div class="container-fluid">
+          <div class="section-header">
+            <h1>
+              Want
+              <small class="light">to talk?</small>
+           </h1>
+          </div>
+          <p class="lead text-center">
+Need something designed, or photographed?  Need a custom illustration for a campaign you are working on or want your wedding photographed? We would love to hear from you, please contact us using one of the methods below.
+          </p>
+          <hr>
+          <div class="row-fluid">
+            <div class="span6">
+<?php  
+  
+        // check for a successful form post  
+        if (isset($_GET['s'])) echo "<div class=\"alert alert-success\">".$_GET['s']."</div>";  
+  
+        // check for a form error  
+        elseif (isset($_GET['e'])) echo "<div class=\"alert alert-error\">".$_GET['e']."</div>";  
+  
+?> 
+              <div id="messages"></div>
+            </div>
+            <div class="span6 contact-details">
+              <ul class="icons">
+                <li>
+                  <h4>
+                    <i class="icon-map-marker"></i>
+                    
+                  </h4>
+                  <p>
+              West Chezzetcook<br>
+	      Nova Scotia
+            </p>
+                  </p>
+                </li>
+                <li>
+                  <h4>
+                    <i class="icon-phone"></i>
+                    Contact details
+                  </h4>
+                  <p>
+		    Phone: 902.482.0593
+                    <br>
+                    Email:  <a href="mailto:info@scsquared.ca">info@scsquared.ca</a>
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+</div>
+</div>
+  <!-- Page Footer -->
+  <footer id="footer" role="contentinfo">
+    <div class="wrapper wrapper-transparent">
+      <div class="container-fluid">
+        <div class="row-fluid">
+          <div class="span6 small-screen-center">
+            <h3>
+              SC&#178;
+            </h3>
+            <p>
+              902.482.0593  |  info@scsquared.ca
+              <br>
+              West Chezzetcook
+              &copy; Copyright 2013
+            </p>
+          </div>
+          <div class="span6">
+            <ul class="unstyled inline text-right small-screen-center big social-icons">
+              <li>
+                <a data-iconcolor="#00a0d1" href="#">
+                  <i class="icon-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a data-iconcolor="#3b5998" href="#">
+                  <i class="icon-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a data-iconcolor="#910101" href="#">
+                  <i class="icon-google-plus"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="javascripts/jquery.min.js" type="text/javascript"></script>
+  <script src="javascripts/bootstrap.js" type="text/javascript"></script>
+  <script src="javascripts/jquery.flexslider-min.js" type="text/javascript"></script>
+  <script src="javascripts/jquery.tweet.js" type="text/javascript"></script>
+  <script src="javascripts/jquery.fancybox.pack.js" type="text/javascript"></script>
+  <script src="javascripts/jquery.fancybox-media.js" type="text/javascript"></script>
+  <script src="javascripts/script.js" type="text/javascript"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42172569-1', 'scsquared.ca');
+  ga('send', 'pageview');
+
+</script>
+</body>
